9ce4faccb49ff601b1313b5d510590ebea731d55
Raimonds Nalezitijs <102956031+RaymondNalezitij@users.noreply.github.com>
2022-09-16 18:41:39 +0300

    Created tests for step 2

--- tests/FooBarQixTest.php
+++ tests/FooBarQixTest.php
@@ -2,22 +2,37 @@
 
 use App\FooBarQix;
 
-test("If given number is multiple of 3 the program should return Foo", function () {
+test("If given number is multiple of 3 the program should return - Foo", function () {
     $number = new FooBarQix(3);
     expect($number->replaceSpecialNumbers())->toEqual("Foo");
 });
 
-test("If given number is multiple of 5 the program should return Bar", function () {
+test("If given number is multiple of 5 the program should return - Bar", function () {
     $number = new FooBarQix(5);
     expect($number->replaceSpecialNumbers())->toEqual("Bar");
 });
 
-test("If the number has several multiples, they appear in natural order (Foo, Bar).", function () {
+test("If given number is multiple of 5 the program should return - Qix", function () {
+    $number = new FooBarQix(7);
+    expect($number->replaceSpecialNumbers())->toEqual("Qix");
+});
+
+test("If the number is multiple of both 3 & 5 but not 7, the program should return - Foo, Bar", function () {
     $number = new FooBarQix(15);
     expect($number->replaceSpecialNumbers())->toEqual("Foo, Bar");
 });
 
-test("If given number is not a multiple of 3 or 5 the program shoud return the number as string", function () {
+test("If the number is multiple of both 3 & 7 but not 5, the program should return - Foo, Qix", function () {
+    $number = new FooBarQix(21);
+    expect($number->replaceSpecialNumbers())->toEqual("Foo, Qix");
+});
+
+test("If the number is multiple of both 5 & 7 but not 3, the program should return - Bar, Qix", function () {
+    $number = new FooBarQix(35);
+    expect($number->replaceSpecialNumbers())->toEqual("Foo, Qix");
+});
+
+test("If given number is not a multiple of 3, 5 or 7 the program should return the number as string", function () {
     $number = new FooBarQix(4);
     expect($number->replaceSpecialNumbers())->toEqual("4");
 });
